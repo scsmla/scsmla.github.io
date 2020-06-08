cf24340b2f8ad5f3bfa422021e294c735bcf7578
oBuiNhatDuy <bui.nhat.duy@sun-asterisk.com>
2020-06-08 12:09:29 +0700

    add index.html

---
+++ precache-manifest.1b90b0a681d5d567f8113c8107d0faee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c225920abac347386997df92189277",
+    "url": "/chmod-caculator/index.html"
+  },
+  {
+    "revision": "1f87fafb6ab0c2457bb4",
+    "url": "/chmod-caculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0d9ef35da2866950ee3",
+    "url": "/chmod-caculator/static/js/2.057e378e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chmod-caculator/static/js/2.057e378e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f87fafb6ab0c2457bb4",
+    "url": "/chmod-caculator/static/js/main.57bafca6.chunk.js"
+  },
+  {
+    "revision": "1b78cb0878ff5e141511",
+    "url": "/chmod-caculator/static/js/runtime-main.f50dc27e.js"
+  }
+]);
